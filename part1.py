b30def722d7a335331613ef9fc867e997fb9d735
Marina Tronina <myssya@gmail.com>
2024-07-21 18:38:19 +0300

    add patr1

---
+++ part1.py
@@ -0,0 +1,18 @@
+#третье правило solid
+
+class Bird():
+    def fly(self):
+        print('Эта птица летает')
+
+class Duck(Bird):
+    def fly(self):
+        print('Эта утка летает быстро')
+
+def fly_in_the_sky(animal):
+    animal.fly()
+
+b = Bird()
+d = Duck()
+
+fly_in_the_sky(b)
+fly_in_the_sky(d)
