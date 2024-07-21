842ba7162bcd08a84038bb458cc919079d5f86ad
Marina Tronina <myssya@gmail.com>
2024-07-21 18:43:56 +0300

    add  part2

---
+++ part2.py
@@ -0,0 +1,22 @@
+# class SmartHouse():
+#     def turn_on_light(self):
+#         pass
+#
+#     def heat_food(self):
+#         pass
+#
+#     def turn_on_music(self):
+#         pass
+
+class Light():
+    def turn_on_light(self):
+        print('свет включен')
+
+class Food():
+    def heat_food(self):
+        print('еда начала разогреваться')
+
+class Music():
+    def turn_on_music(self):
+        print('Включаю ваш плей лист')
+
